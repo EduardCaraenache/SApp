bb1be49da67053ce8fb780c9be9c5089a3a4e61f
Eduuu <92028565+EduardCaraenache@users.noreply.github.com>
2022-05-01 13:58:25 +0300

    Adaugat pagini

---
+++ client/src/pages/jobs/Jobs.jsx
@@ -0,0 +1,19 @@
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import "./jobs.css";
+
+export default function Groups() {
+  return (
+    <>
+      <Topbar />
+      <div className="jobsContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+      <Sidebar />
+    </>
+  );
+}
---
+++ client/src/pages/videos/Videos.jsx
@@ -0,0 +1,19 @@
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import "./videos.css";
+
+export default function Groups() {
+  return (
+    <>
+      <Topbar />
+      <div className="videosContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+      <Sidebar />
+    </>
+  );
+}
